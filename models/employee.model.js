c0c016a14f0e984a2c9dda76d0a13d2340f2951c
Patryk Olszewski <patrykolszewski420@gmail.com>
2025-05-10 14:43:27 +0200

    controllers added

--- models/employee.model.js
+++ models/employee.model.js
@@ -1,8 +1,13 @@
 const mongoose = require("mongoose");
 
-const employeesSchema = new mongoose.Schema({
-	_id: { type: mongoose.Types.ObjectId, required: true },
+const employeeSchema = new mongoose.Schema({
 	firstName: { type: String, required: true },
 	lastName: { type: String, required: true },
-	department: { type: String, required: true },
+	department: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: "Department",
+		required: true,
+	},
 });
+
+module.exports = mongoose.model("Employee", employeeSchema);
