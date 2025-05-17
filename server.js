d15ffd47ef1e26a4dc22ec891b6023a102d2f977
Patryk Olszewski <patrykolszewski420@gmail.com>
2025-05-17 14:28:07 +0200

    basic security measures

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
 
-const employeesRoutes = require('./routes/employees.routes');
-const departmentsRoutes = require('./routes/departments.routes');
-const productsRoutes = require('./routes/products.routes');
+const employeesRoutes = require("./routes/employees.routes");
+const departmentsRoutes = require("./routes/departments.routes");
+const productsRoutes = require("./routes/products.routes");
 
 const app = express();
 
@@ -12,22 +12,30 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/api', employeesRoutes);
-app.use('/api', departmentsRoutes);
-app.use('/api', productsRoutes);
+app.use("/api", employeesRoutes);
+app.use("/api", departmentsRoutes);
+app.use("/api", productsRoutes);
 
 app.use((req, res) => {
-  res.status(404).send({ message: 'Not found...' });
-})
+	res.status(404).send({ message: "Not found..." });
+});
 
-mongoose.connect('mongodb://0.0.0.0:27017/companyDB', { useNewUrlParser: true });
+const NODE_ENV = process.env.NODE_ENV;
+let dbUri = "";
+
+if (NODE_ENV === "production") dbUri = "url to remote db";
+else if (NODE_ENV === "test") dbUri = "mongodb://0.0.0.0:27017/companyDBtest";
+else dbUri = "mongodb://0.0.0.0:27017/companyDB";
+
+mongoose.connect(dbUri, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
-
-db.once('open', () => {
-  console.log('Connected to the database');
+db.once("open", () => {
+	console.log("Connected to the database");
 });
-db.on('error', err => console.log('Error ' + err));
+db.on("error", (err) => console.log("Error " + err));
 
-app.listen('8000', () => {
-  console.log('Server is running on port: 8000');
+const server = app.listen("8000", () => {
+	console.log("Server is running on port: 8000");
 });
+
+module.exports = server;
